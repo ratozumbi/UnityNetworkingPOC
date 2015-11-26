927411cf376e60b1caefead6e6a146553387e06d
Bruno Willian Andrade <brunowillianandrade@gmail.com>
2015-11-26 03:48:08 -0200

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class GameManager : NetworkBehaviour {
+
+	// Use this for initialization
+	public void StartGame () {
+		NetworkManager.singleton.StartHost ();
+	}
+	
+	// Update is called once per frame
+	public void JoinGame () {
+		NetworkManager.singleton.StartClient ();
+	}
+}
---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+	public Camera fpsCamera;
+	public AudioListener fpsAudioListner;
+	// Use this for initialization
+	void Start () {
+
+		if (!isLocalPlayer) {
+			return;
+		}
+		GameObject.Find ("Main Camera").SetActive(false);
+		GetComponent<CharacterController> ().enabled = true;
+		GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController> ().enabled = true;
+		GetComponent<AudioSource> ().enabled = true;
+
+		fpsCamera.enabled = true;
+		fpsAudioListner.enabled = true;
+	}
+}
